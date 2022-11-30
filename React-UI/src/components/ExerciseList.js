4502a635bbd6ba968d541225ab074c2bd945301d
arman-man <84875686+arman-man@users.noreply.github.com>
2022-11-29 18:08:54 -0800

    Upload React-UI

---
+++ React-UI/src/components/ExerciseList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Exercise from './Exercise';
+
+function ExerciseList({ exercises, onDelete, onEdit }) {
+    return (
+        <table id="exercises">
+            <caption>Table of Exercises</caption>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>reps</th>
+                    <th>weight</th>
+                    <th>unit</th>
+                    <th>date</th>
+                    <th>Delete</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                {exercises.map((exercise, i) =>
+                    <Exercise
+                        exercise={exercise}
+                        key={i}
+                        onDelete={onDelete}
+                        onEdit={onEdit}
+                    />)}
+            </tbody>
+        </table>
+    );
+}
+
+export default ExerciseList;
